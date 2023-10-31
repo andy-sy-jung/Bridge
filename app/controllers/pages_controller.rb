6e327b2dac1490f23362c507e3c83234a3dd82b7
Andy <andyjung5591@gmail.com>
2023-10-31 17:34:23 -0400

    testing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
     
     def index
         if session[:loggedIn].nil? or session[:loggedIn] == False
-            redirect_to signup_path
+            redirect_to "devise/registration/new"
         end
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
    root "pages#index"
-
-   get "devise/signup" => "devise#signup", as: 'signup'
   
 end
