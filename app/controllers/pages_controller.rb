a8b750ff6fae8b07dc2780169d431919c9f7b038
Andy <andyjung5591@gmail.com>
2023-10-31 18:04:32 -0400

    logout button

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
     
     def index
         session[:loggedIn] = params[:loggedIn]
-        if session[:loggedIn].nil?
+        if session[:loggedIn].nil? or session[:loggedIn] == False
             redirect_to new_user_session_path
         end
     end
