11431ea7e7a877e03091f50c055c49f3a68b1b3d
andy-sy-jung <67531241+andy-sy-jung@users.noreply.github.com>
2023-12-18 23:52:47 -0500

    Update application_spec.rb

--- spec/mailers/application_spec.rb
+++ spec/mailers/application_spec.rb
@@ -1,9 +1,9 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe ApplicationMailer, type: :mailer do
+# RSpec.describe ApplicationMailer, type: :mailer do
 
-  describe 'email sending' do
-    let(:user) { create(:user, email: 'user@example.com') }
+#   describe 'email sending' do
+#     let(:user) { create(:user, email: 'user@example.com') }
 
-  end
-end
+#   end
+# end
