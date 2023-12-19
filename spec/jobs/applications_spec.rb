c0a66344bd243d18d097656ccdc26afc89ab23e8
andy-sy-jung <67531241+andy-sy-jung@users.noreply.github.com>
2023-12-18 23:47:48 -0500

    Update applications_spec.rb

--- spec/jobs/applications_spec.rb
+++ spec/jobs/applications_spec.rb
@@ -1,17 +1,17 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe ApplicationJob, type: :job do
-  describe 'retry and discard behavior' do
-    it 'retries on ActiveRecord::Deadlocked' do
-      # Uncomment the relevant retry_on line in your ApplicationJob
-      # to make this test meaningful
-      # retry_job { expect(subject).to be_a(ActiveRecord::Deadlocked) }
-    end
+# RSpec.describe ApplicationJob, type: :job do
+#   describe 'retry and discard behavior' do
+#     it 'retries on ActiveRecord::Deadlocked' do
+#       # Uncomment the relevant retry_on line in your ApplicationJob
+#       # to make this test meaningful
+#       # retry_job { expect(subject).to be_a(ActiveRecord::Deadlocked) }
+#     end
 
-    it 'discards on ActiveJob::DeserializationError' do
-      # Uncomment the relevant discard_on line in your ApplicationJob
-      # to make this test meaningful
-      # discard_job { expect(subject).to be_a(ActiveJob::DeserializationError) }
-    end
-  end
-end
+#     it 'discards on ActiveJob::DeserializationError' do
+#       # Uncomment the relevant discard_on line in your ApplicationJob
+#       # to make this test meaningful
+#       # discard_job { expect(subject).to be_a(ActiveJob::DeserializationError) }
+#     end
+#   end
+# end
